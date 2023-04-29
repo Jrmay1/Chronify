2cc0f12bcb38fc12f016f5eca5e3d35abaf230d0
Jeremy May <jeremyrogermay@outlook.com>
2023-04-29 12:43:05 +0100

    Set Setup scripts

--- Setup.sh
+++ Setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Fetch Latest
+git fetch
+git checkout master
+git pull
 
+# Copy Apache Files to required location
+cp chronify.net.conf /etc/apache2/sites-enabled/chronify.net.conf
+
+# Copy website to required location
+rm -rf /var/www/chronify.net/publish
+cp -r publish /var/www/chronify.net/
+
+# Restart Apache Server
+sudo systemctl restart apache2
