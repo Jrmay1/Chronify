e61bfb667da3ffbb73904057aef0bb084198e8f4
Jeremy May <jeremyrogermay@outlook.com>
2023-04-29 12:55:51 +0100

    finalise setup script

--- Setup.sh
+++ Setup.sh
@@ -6,11 +6,11 @@ git checkout master
 git pull
 
 # Copy Apache Files to required location
-cp chronify.net.conf /etc/apache2/sites-enabled/chronify.net.conf
+sudo cp chronify.net.conf /etc/apache2/sites-enabled/chronify.net.conf
 
 # Copy website to required location
-rm -rf /var/www/chronify.net/publish
-cp -r publish /var/www/chronify.net/
+sudo rm -rf /var/www/chronify.net/publish
+sudo cp -r publish /var/www/chronify.net/
 
 # Restart Apache Server
 sudo systemctl restart apache2
